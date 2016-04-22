4c8c887323f45c1751f2a7081977aee41b230d6f
alexis <zvin@free.fr>
2016-04-22 19:42:11 +0100

    fix router allow_migration

--- redmine_models/routers.py
+++ redmine_models/routers.py
@@ -6,15 +6,18 @@ from django.conf import settings
 
 
 class DatabaseRouter(object):
+    app_name = "redmine_models"
 
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == "redmine_models":
+        if model._meta.app_label == self.app_name:
             return settings.REDMINE_DATABASE
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == "redmine_models":
+        if model._meta.app_label == self.app_name:
             return settings.REDMINE_DATABASE
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
-        if db == settings.REDMINE_DATABASE and app_label != "redmine_models":
+        if db == settings.REDMINE_DATABASE:
+            return app_label == self.app_name
+        elif app_label == self.app_name:
             return False
